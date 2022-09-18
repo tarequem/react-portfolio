7bcae9a860f9afa10bbde9160f972c895111dcb4
tarequem <moatartareque@gmail.com>
2022-09-18 19:04:57 -0400

    added screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,26 @@ module.exports = {
       "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-      extend: {
-        colors: {
-            'regal-blue': '#243c5a',
-          },
+        screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+      
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+      
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+      
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+      
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        },
+        extend: {
+            colors: {
+                primary: '#243c5a',
+            },
       },
     },
     plugins: [],
